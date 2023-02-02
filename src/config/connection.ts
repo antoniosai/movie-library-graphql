fb8d1ef5689caf14bc22c1014405d19fa28f685d
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-02-02 11:30:51 +0700

    [update] change db connection

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,5 +1,18 @@
-import { Sequelize } from "sequelize-typescript";
+import { Dialect, Sequelize } from 'sequelize';
 
-const connection = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+const dbDriver = "postgres" as Dialect
+const dbHost = "127.0.0.1"
+const dbPort = 5439;
+const dbName = "db_movie" as string
+const dbUser = "myusername" as string
+const dbPassword = "mypassword"
 
-export default connection;
+console.log("ENV => ", process.env);
+
+const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  port: dbPort,
+  dialect: dbDriver
+})
+
+export default sequelizeConnection
