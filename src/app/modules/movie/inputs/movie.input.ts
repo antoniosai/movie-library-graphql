2c197e722a445e8f777a815038a6464870f84338
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-02-02 19:43:58 +0700

    [add] create an input for movie entity

---
+++ src/app/modules/movie/inputs/movie.input.ts
@@ -0,0 +1,21 @@
+import { MaxLength } from "class-validator";
+import { Field, InputType } from "type-graphql";
+import { MovieDTO } from "../dto/movie.dto";
+
+@InputType()
+export class MovieInput implements Omit<MovieDTO, 'idMovie'> {
+
+  @Field()
+  @MaxLength(30)
+  name: string;
+
+  genre: "mystery" | "thriller" | "comedy" | "sci-fi";
+
+  @Field()
+  @MaxLength(4)
+  year: number;
+  
+  @Field()
+  idAuthor: number;
+
+}
