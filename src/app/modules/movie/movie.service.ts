63c692a413366fffa607ca643e150651776517eb
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-02-02 19:44:20 +0700

    [add] create a service for movie

---
+++ src/app/modules/movie/movie.service.ts
@@ -0,0 +1,32 @@
+import { MovieDTO } from "./dto/movie.dto";
+import { MovieInput } from "./inputs/movie.input";
+import MovieModel from "./movie.model";
+
+export class MovieService {
+
+  async getAllMovies(): Promise<MovieDTO[]> {
+    console.log("Fetching Data from User");
+    return await MovieModel.findAll();
+  }
+
+  async getMovieById(idMovie: number): Promise<MovieDTO | null> {
+    const response: MovieDTO | null = await MovieModel.findByPk(idMovie);
+
+    return response;
+  }
+
+  async insertNewMovie(movieInput: MovieInput): Promise<MovieDTO> {
+    const response: MovieDTO | null = await MovieModel.create(movieInput);
+
+    return response;
+  }
+
+  async updateMovie(idMovie: number, movieInput: MovieInput): Promise<[affectedCount: number]> {
+    const response: [affectedCount: number] = await MovieModel.update(movieInput, {
+      where: { idMovie: idMovie }
+    });
+
+    return response;
+  }
+
+}
